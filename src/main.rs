9c6e8d8c4a4d4bb4180b3ab6576411bb616cdf1b
Linus Färnstrand <faern@faern.net>
2016-03-14 21:09:50 +0100

    Adapt to work with new design

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use rips::NetworkStack;
 
 fn main() {
     println!("Hello, world!");
-    let ifaces = NetworkStack::get_network_interfaces();
+    let ifaces = rips::get_network_interfaces();
     let mut iface = None;
     for curr_iface in ifaces.into_iter() {
         println!("iface: {:?}", curr_iface);
@@ -23,19 +23,23 @@ fn main() {
     let iface = iface.unwrap();
     let mut stack = NetworkStack::new(vec![iface.clone()])
                         .expect("Expected a working NetworkStack");
-    let ethernet = stack.get_ethernet(&iface).expect("Expected Ethernet");
-    let mut i = 1;
-    ethernet.send(5, 20, |pkg| {
-        pkg.set_source(MacAddr::new(0x10, 0x11, 0x12, 0x13, 0x14, 0x15));
-        pkg.set_destination(MacAddr::new(5, 6, 7, 8, 9, 4));
-        pkg.set_ethertype(EtherType::new(0x1337));
-        pkg.set_payload(vec![i, i + 1]);
-        i += 1
-    });
+    // let ethernet = stack.get_ethernet(&iface).expect("Expected Ethernet");
+    // let mut i = 1;
+    // ethernet.send(5, 20, |pkg| {
+    //     pkg.set_source(MacAddr::new(0x10, 0x11, 0x12, 0x13, 0x14, 0x15));
+    //     pkg.set_destination(MacAddr::new(5, 6, 7, 8, 9, 4));
+    //     pkg.set_ethertype(EtherType::new(0x1337));
+    //     pkg.set_payload(vec![i, i + 1]);
+    //     i += 1
+    // });
 
-    ethernet.send_arp(|pkg| {
-        pkg.set_target_ip(Ipv4Addr::new(10, 0, 0, 1));
-    });
+    let ip = Ipv4Addr::new(10, 0, 0, 1);
+    let arp = stack.get_arp(&iface).expect("Expected arp");
+    {
+        let mut arp = arp.lock().unwrap();
+        let mac = arp.get(&ip);
+        println!("MAC {} belongs to {}", mac, ip);
+    }
 
     thread::sleep(Duration::new(1, 0));
 }
