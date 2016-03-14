181088449a1a0464fe72532e9edf742ceea4c3d1
Linus Färnstrand <faern@faern.net>
2016-03-14 22:46:47 +0100

    Sending an IP packet works

--- src/main.rs
+++ src/main.rs
@@ -23,22 +23,35 @@ fn main() {
     let iface = iface.unwrap();
     let mut stack = NetworkStack::new(vec![iface.clone()])
                         .expect("Expected a working NetworkStack");
-    // let ethernet = stack.get_ethernet(&iface).expect("Expected Ethernet");
-    // let mut i = 1;
-    // ethernet.send(5, 20, |pkg| {
-    //     pkg.set_source(MacAddr::new(0x10, 0x11, 0x12, 0x13, 0x14, 0x15));
-    //     pkg.set_destination(MacAddr::new(5, 6, 7, 8, 9, 4));
-    //     pkg.set_ethertype(EtherType::new(0x1337));
-    //     pkg.set_payload(vec![i, i + 1]);
-    //     i += 1
-    // });
+    let eth = stack.get_ethernet(&iface).expect("Expected Ethernet");
+    {
+        let mut eth = eth.lock().unwrap();
+        let mut i = 1;
+        eth.send(5, 20, |pkg| {
+            pkg.set_source(MacAddr::new(0x10, 0x11, 0x12, 0x13, 0x14, 0x15));
+            pkg.set_destination(MacAddr::new(5, 6, 7, 8, 9, 4));
+            pkg.set_ethertype(EtherType::new(0x1337));
+            pkg.set_payload(vec![i, i + 1]);
+            i += 1
+        });
+    }
 
-    let ip = Ipv4Addr::new(10, 0, 0, 1);
+    let dst_ip = Ipv4Addr::new(10, 0, 0, 1);
     let arp = stack.get_arp(&iface).expect("Expected arp");
     {
         let mut arp = arp.lock().unwrap();
-        let mac = arp.get(&ip);
-        println!("MAC {} belongs to {}", mac, ip);
+        let mac = arp.get(&dst_ip);
+        println!("MAC {} belongs to {}", mac, dst_ip);
+        let mac2 = arp.get(&dst_ip);
+        println!("Second time MAC {} belongs to {}", mac2, dst_ip);
+    }
+
+    let ipv4 = stack.get_ipv4(&iface).expect("Expected ipv4");
+    {
+        let mut ipv4 = ipv4.lock().unwrap();
+        ipv4.send(&dst_ip, 10, |pkg| {
+            pkg.set_payload(vec![0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19]);
+        });
     }
 
     thread::sleep(Duration::new(1, 0));
